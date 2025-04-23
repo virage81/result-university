a819a9f8c594c439f0c1355932a95d590f1459a2
virage81 <karimovminds@gmail.com>
2025-04-23 21:27:08 +0400

    feat: add CopyPlugin and favicon

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,5 +1,6 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
 	context: path.resolve(__dirname, "src"),
@@ -12,6 +13,14 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, "public/index.html"),
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, "public/favicon.ico"),
+					to: path.resolve(__dirname, "dist"),
+				},
+			],
 		}),
 	],
 	module: {
