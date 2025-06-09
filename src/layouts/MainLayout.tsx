1587ae4150cca35a45cffccb7e2b6eb4b2ca87c9
virage81 <karimovminds@gmail.com>
2025-06-09 14:59:35 +0400

    chore: add mantine-ui pkg

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,19 +4,24 @@ import { Header } from '@/components';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { PageLoading } from '@/components/PageLoading';
 import { AuthProvider } from '@/contexts/auth';
+import { createTheme, MantineProvider } from '@mantine/core';
 import { Suspense } from 'react';
+
+const theme = createTheme({});
 
 export const MainLayout = () => {
 	return (
-		<AuthProvider>
-			<Header />
-			<main className='flex w-full p-5 mx-auto max-w-7xl grow'>
-				<ErrorBoundary>
-					<Suspense fallback={<PageLoading />}>
-						<Outlet />
-					</Suspense>
-				</ErrorBoundary>
-			</main>
-		</AuthProvider>
+		<MantineProvider theme={theme}>
+			<AuthProvider>
+				<Header />
+				<main className='flex w-full p-5 mx-auto max-w-7xl grow'>
+					<ErrorBoundary>
+						<Suspense fallback={<PageLoading />}>
+							<Outlet />
+						</Suspense>
+					</ErrorBoundary>
+				</main>
+			</AuthProvider>
+		</MantineProvider>
 	);
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import '@mantine/core/styles.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
